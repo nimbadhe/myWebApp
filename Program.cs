5511fc62c587ec9c2edf8e8d6afec57adc00262d
nimbadhe <kriips99@hotmail.com>
2025-01-09 14:53:24 +0200

    change2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. Change2
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
